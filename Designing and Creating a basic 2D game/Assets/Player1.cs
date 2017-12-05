a90b300de947d1eb608e9c76c5c123a489896fca
MalcolmSammut <malcolm.sammut.b31226@mcast.edu.mt>
2017-12-05 15:28:25 +0100

    Updated Main Menu
    Player 1 and player 2 move and collision with the border.

--- Designing and Creating a basic 2D game/Assets/Player1.cs
+++ Designing and Creating a basic 2D game/Assets/Player1.cs
@@ -14,7 +14,13 @@ public class Player1 : MonoBehaviour {
 		//this should print in the console the position of the cursor
 		float mousePosInBlocks = (Input.mousePosition.y / Screen.width * 8) - 4f;
 		Vector3 paddlePosition = new Vector3(this.transform.position.x, mousePosInBlocks , 0f);
+		if (paddlePosition.y > 2.2f)
+			paddlePosition.y = 2.2f;
+		else if (paddlePosition.y < -3.3f)
+				paddlePosition.y = -3.3f;
+
 		this.transform.position = paddlePosition;
+
 		//-4f stands for half the screen width in units. This is done so that the mouse and paddle move underneath each other
 
 	}
