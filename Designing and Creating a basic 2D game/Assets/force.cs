2a9df969f203c3b41407c7a22b03f23729cc77a9
MalcolmSammut <malcolm.sammut.b31226@mcast.edu.mt>
2017-12-07 15:58:55 +0100

    Moving Ball
    The ball is moving  when hit player 1 or player 2 move the other way.

--- Designing and Creating a basic 2D game/Assets/force.cs
+++ Designing and Creating a basic 2D game/Assets/force.cs
@@ -3,18 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class force : MonoBehaviour {
-	private Rigidbody rb;
+	private Rigidbody2D rb;
+
 	// Use this for initialization
 	void Start () {
-		rb = GetComponent<Rigidbody> ();
+		int rand = Random.Range (0, 2);
+		if (rand == 0) {
+			this.GetComponent<Rigidbody2D> ().velocity = new Vector2 (-8f, 0f);
+		}
+		else if(rand==1){
+			this.GetComponent<Rigidbody2D> ().velocity = new Vector2 (8f, 0f);
+		}
 	}
 	
 	// Update is called once per frame
-	void FixedUpdate () {
-		float moveHorizontal = Input.GetAxis ("Horzontal");
-		float moveVertical = Input.GetAxis ("Vertical");
-
-		Vector3 movement = new Vector3 (moveHorizontal, 0.0F, moveVertical);
-		rb.AddForce (movement);
+	void Update () {
+		
+	}
+	void onCollisionEnter2D(Collider2D hit){
+		float distance = this.transform.position.y - GameObject.Find ("Player1").transform.position.y;
+		float distance2 = this.transform.position.y - GameObject.Find ("Player2").transform.position.y;
+		if (hit.gameObject.name=="Player1") {
+			this.GetComponent<Rigidbody2D> ().velocity = new Vector2 (8f,distance*2f);
+		}
+		if(hit.gameObject.name=="Player2"){
+			this.GetComponent<Rigidbody2D> ().velocity = new Vector2 (-8f,distance2*2f);
+		}
 	}
 }
